0c26dab8367f47f68c1c993276aa72c98ebaa865
Vikas <vikas@tradala.in>
2019-01-11 19:03:10 +0530

    added transaction code

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -2,13 +2,14 @@ pragma solidity ^0.5.0;
 
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20Detailed.sol";
+import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
 
-contract MyToken is ERC20, ERC20Detailed {
-    
-    constructor(string memory _name, string memory _symbol, uint8 _decimals)
-        ERC20Detailed(_name, _symbol, _decimals)
-        public
-    {
+contract MyToken is ERC20, ERC20Detailed, Ownable {
+    uint256 public constant INITIAL_SUPPLY = 1000000000000000;
 
+    constructor () public ERC20Detailed("MyToken", "VRK",18){
+        _mint(msg.sender, INITIAL_SUPPLY);
+        emit Transfer(address(0), msg.sender, totalSupply());
     }
+
 }
--- test/MyToken.test.js
+++ test/MyToken.test.js
@@ -1,29 +1,33 @@
-const BigNumber = web3.BigNumber;
+// const web3 = require('web3');
 const MyToken = artifacts.require("./MyToken");
+// let BigNumber = require('bignumber.js');
+const BigNumber = web3.BigNumber;
+
+// const BigNumber = web3.BigNumber;
 
 require('chai')
     .use(require('chai-bignumber')(BigNumber))
     .should();
 
 contract('MyToken', accounts => {
-    const _name = "My Token";
+    const _name = "MyToken";
     const _symbol = "VRK";
     const _decimals = 18;
     beforeEach(async function () {
-        this.token = await MyToken.new(_name, _symbol, _decimals);
+        this.token = await MyToken.new();
     })
     describe('token attributes', function () {
         it('has the correct name', async function () {
             const name = await this.token.name();
-            name.should.equals(_name);
+            name.should.equal(_name);
         });
         it('has the correct symbol', async function () {
             const symbol = await this.token.symbol();
-            symbol.should.equals(_symbol);
+            symbol.should.equal(_symbol);
         });
         it('has the correct decimals', async function () {
             const decimals = await this.token.decimals();
-            decimals.should.be.bignumber.equals(_decimals);
+            decimals.should.be.bignumber.equal(_decimals);
         });
     });
 })
